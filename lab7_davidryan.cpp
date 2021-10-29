0295553fee7e993cbb73d290e469df7dd7de348b
anavid7 <42363980+anavid7@users.noreply.github.com>
2021-10-29 16:01:12 -0500

    Update lab7_davidryan.cpp

--- lab7_davidryan.cpp
+++ lab7_davidryan.cpp
@@ -1,11 +1,14 @@
 /**
  * lab7_davidryan.cpp
  * author: David ryan
- * Date: 10/29/2021
+ * class: Numerical Analysis CSCE 440
+ * date: 10/29/2021
  * 
  * Calculates the integration of the 
  * function x^2/4 + y^2 = 1 from -1 to 1
  * using the Monte Carlo method.
+ * 
+ * compile with: g++ lab7_davidryan.cpp -std=c++11
 */
 
 #include <cstdio>
